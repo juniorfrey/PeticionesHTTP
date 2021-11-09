9847d6fbbe83e7995bc58b3436287302e4a16ce1
Fredys Fernandez <juniorfrey2990@gmail.com>
2021-11-09 13:48:22 -0500

    Clase-2 Promesas en cadena

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@
 
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-fetch( jokeUrl ).then( resp => {
+/*fetch( jokeUrl ).then( resp => {
     //resp.json().then(data) => {
     resp.json().then(({id, value}) => {
         //console.log(data);
         console.log(id);
         console.log(value);
     })
-})
+})*/
+
+fetch ( jokeUrl ).then(resp => resp.json() )
+      .then( ({id, value}) => {
+          console.log(id);
+          console.log(value);
+      } );
